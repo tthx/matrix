f3fb0b852b368e76b1004b5158036e119ec2c619
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2019-09-19 12:54:00 +0200

    0.1

--- src/main/scala/com/orange/tgi/ols/arsec/paas/aacm/matrix/Multiply.scala
+++
@@ -1,5 +0,0 @@
-package com.orange.tgi.ols.arsec.paas.aacm.matrix
-
-object Multiply extends App {
-  println("Hello")
-}
--- src/test/scala/com/orange/tgi/ols/arsec/paas/aacm/matrix/MultiplySpec.scala
+++
@@ -1,9 +0,0 @@
-package com.orange.tgi.ols.arsec.paas.aacm.matrix
-
-import org.scalatest._
-
-class MultiplySpec extends FunSuite with DiagrammedAssertions {
-  test("Hello should start with H") {
-    assert("Hello".startsWith("H"))
-  }
-}
