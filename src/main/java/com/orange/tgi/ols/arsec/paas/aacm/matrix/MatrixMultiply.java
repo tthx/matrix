153a34bd3fc77d3d8128793a8a413a43afe7f43c
yellowred-orange <thaihoa.trinh@orange.com>
2019-09-20 11:31:28 +0000

    0.1

--- src/main/java/com/orange/tgi/ols/arsec/paas/aacm/matrix/MatrixMultiply.java
+++ src/main/java/com/orange/tgi/ols/arsec/paas/aacm/matrix/MatrixMultiply.java
@@ -1,10 +1,43 @@
 package com.orange.tgi.ols.arsec.paas.aacm.matrix;
 
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+// RowMatrix
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.linalg.distributed.RowMatrix;
+// IndexedRowMatrix
+import org.apache.spark.mllib.linalg.distributed.IndexedRow;
+import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
+// CoordinateMatrix
+import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
+import org.apache.spark.mllib.linalg.distributed.CoordinateMatrix;
+// BlockMatrix
+import org.apache.spark.mllib.linalg.distributed.BlockMatrix;
+
 public class MatrixMultiply {
 
+  
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    SparkConf conf = new SparkConf().setAppName("Matrix Multiplication");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+  }
+  
+  public void createRowMatrix(JavaSparkContext sc, int n, int m) {
+    JavaRDD<Vector> rows = sc.parallelize(Vectors.zeros(n*m));
+  }
+  
+  public void createIndexedRowMatrix(int n, int m) {
+    
+  }
+  
+  public void createCoordinateMatrix(int n, int m) {
+    
+  }
+  
+  public void createBlockMatrix(int n, int m) {
+    
   }
 
 }
