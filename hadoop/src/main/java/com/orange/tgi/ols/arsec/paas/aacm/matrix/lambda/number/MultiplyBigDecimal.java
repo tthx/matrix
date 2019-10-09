668176fe76d663df13620d4c3e600855f2444e17
Trinh Thai Hoa <thaihoa.trinh@orange.com>
2019-10-09 13:02:12 +0200

    0.1

---
+++ hadoop/src/main/java/com/orange/tgi/ols/arsec/paas/aacm/matrix/lambda/number/MultiplyBigDecimal.java
@@ -0,0 +1,14 @@
+package com.orange.tgi.ols.arsec.paas.aacm.matrix.lambda.number;
+
+import java.math.BigDecimal;
+
+import com.orange.tgi.ols.arsec.paas.aacm.matrix.lambda.hadoop.MatrixMultiply;
+
+public class MultiplyBigDecimal
+    extends MatrixMultiply<BigDecimal, BigDecimal, BigDecimal> {
+
+  public static void main(String[] args) throws Exception {
+    int res = 1;
+    System.exit(res);
+  }
+}
